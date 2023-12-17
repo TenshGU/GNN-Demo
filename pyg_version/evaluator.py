1774be062ed49e610c41ca62dd3fc6a4f9509d5c
TenshGU <fortgu@163.com>
2023-12-17 15:50:10 +0800

    pyg_version finished

--- pyg_version/evaluator.py
+++ pyg_version/evaluator.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 from data_loader import pyg_loader
 from model import GCN
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
@@ -21,6 +22,8 @@ if __name__ == "__main__":
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
     model.to(device)
 
+    train_accuracies = []  # 用于记录每个训练周期结束后的训练精度
+
     model.train()
     for epoch in range(300):
         optimizer.zero_grad()
@@ -28,7 +31,22 @@ if __name__ == "__main__":
         loss = F.nll_loss(out[idx_train], labels[idx_train])  # 损失函数
         loss.backward()
         optimizer.step()
-        print(f"epoch:{epoch + 1}, loss:{loss.item()}")
+
+        # 在训练过程中计算训练精度并记录
+        _, train_pred = out[idx_train].max(dim=1)
+        correct_train = train_pred.eq(labels[idx_train]).sum()
+        train_acc = int(correct_train) / int(len(idx_train))
+        train_accuracies.append(train_acc)
+
+        print(f"epoch:{epoch + 1}, loss:{loss.item()}, train_acc:{train_acc}")
+
+    # 在训练结束后绘制训练精度图
+    plt.plot(range(1, len(train_accuracies) + 1), train_accuracies, label='Train Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.title('Training Accuracy Over Epochs')
+    plt.legend()
+    plt.show()
 
     model.eval()
     _, pred = model(features, edges).max(dim=1)
